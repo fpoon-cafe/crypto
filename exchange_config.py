1bf8064688608ba6d23708c1867a266efbbd9296
fpoon-cafe <fpoon.cafe@gmail.com>
2021-09-27 21:32:32 -0400

    add example png and gitignore

--- exchange_config.py
+++ exchange_config.py
@@ -1,6 +1,6 @@
 class EXCHANGE_CONFIG:
     # Read-Write Keys
-    KEY = 'ftx key'
-    SECRET = 'ftx secret'
+    KEY = 'ftx key 2'
+    SECRET = 'ftx secret 2'
     CRYPTO_LIST = ['BTC', 'ETH', 'XRP', 'LTC', 'MATIC', 'SOL']
     FUNDING_INTERVAL = 1 #hourly
